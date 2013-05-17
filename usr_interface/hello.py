7705beee88318940e66f7fbb2ec340cecf387809
recursivelab <jasmedia.jas@gmail.com>
2013-05-17 16:54:07 +0530

    avr-project

---
+++ usr_interface/hello.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, jsonify
+import serial
+
+TTY_PORT = '/dev/ttyS0'
+BAUD = 9600
+s = serial.Serial(TTY_PORT, BAUD, timeout=1)
+app = Flask(__name__)
+@app.route('/')
+def main():
+  adc = request.args.get('ADC_COMMAND','1', type=int)
+  if adc== 1:
+    s.write(chr(0x1D))   
+    response = s.read(255)
+    inter = jsonify(response)
+  
+  a = request.args.get('ledred','0', type=int)
+  if a == 1:
+    s.write(chr(0x01))
+  elif a == 0:
+    s.write(chr(0x11))
+  b = request.args.get('ledgreen','0',type=int)
+  if b == 1:
+    s.write(chr(0x03))
+  elif b == 0:
+    s.write(chr(0x1B))
+  c = request.args.get('ledwhite','0',type=int)
+  if c == 1:
+    s.write(chr(0x04))
+  elif c == 0:
+    s.write(chr(0x1C))
+  d = request.args.get('ledyellow','0', type=int)
+  if d == 1:
+    s.write(chr(0x02))
+  elif d == 0:
+    s.write(chr(0x1A))
+ 
+  return render_template('test.html')
+
+s.close()
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
